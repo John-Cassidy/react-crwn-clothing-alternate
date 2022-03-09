9b0af0711d7ce6af94c328ba277fd3e2f9e3375d
John Cassidy <jpcassidy@outlook.com>
2022-03-09 11:51:20 -0500

    create collection.context, cart.context, CurrentUserContext and update app, header, cart-icon, collections-overview, collection components to useContext

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,30 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
-
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
-);
+import React, { useContext } from 'react';
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-});
+import CartContext from '../../contexts/cart/cart.context';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+const CartIcon = ({ itemCount }) => {
+  const { toggleHidden } = useContext(CartContext);
+  return (
+    <div className='cart-icon' onClick={toggleHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount
+  itemCount: selectCartItemsCount,
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CartIcon);
+export default connect(mapStateToProps)(CartIcon);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionsContext from '../../contexts/collections/collections.context';
+import React from 'react';
+
+const CollectionPage = ({ match }) => {
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        );
+      }}
+    </CollectionsContext.Consumer>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+// // alternate
+// const CollectionPage = ({ match }) => {
+//   const collections = useContext(CollectionsContext);
+//   const collection = collections[match.params.collectionId];
+//   const { title, items } = collection;
 
-export default connect(mapStateToProps)(CollectionPage);
+//   return (
+//     <div className='collection-page'>
+//       <h2 className='title'>{title}</h2>
+//       <div className='items'>
+//         {items.map(item => (
+//           <CollectionItem key={item.id} item={item} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+export default CollectionPage;
