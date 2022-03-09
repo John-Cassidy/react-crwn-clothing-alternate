d610b21b3a813d32e8aea94df7b9b90edc27d0e2
John Cassidy <jpcassidy@outlook.com>
2022-03-09 15:36:36 -0500

    delete redux folder and remove redux packages

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 Replace local state management from redux to the new context API.
 
 [Completed Lesson](https://github.com/ZhangMYihua/react-context-complete)
+
+remove redux folder
+create context and provider folders
+
+replace redux
+implement contexts and providers
+
+uninstall redux:
+
+> npm uninstall redux-persist
+> npm uninstall redux-logger
+> npm uninstall redux
+> npm uninstall react-redux
--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import './index.css';
 
-import { persistor, store } from './redux/store';
-
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import CartProvider from './providers/cart/cart.provider';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <CartProvider>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </CartProvider>,
   document.getElementById('root')
 );
